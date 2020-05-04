5846dcaf2377c328a3e1db0cf534e919226b924a
J. Jefferson N. do Vale <criacao.jefferson@gmail.com>
2020-05-03 23:47:39 -0300

    create botton off

--- script.js
+++ script.js
@@ -1,6 +1,10 @@
-function onOff(){
+function onLamp(){
+    var lamp = document.getElementById('lamp');  
+    lamp.innerHTML = `<img src="img/lamp-on.jpg" onclick="onOff()">`   
+}
+
+function offLamp(){
     var lamp = document.getElementById('lamp');
     
-    lamp.innerHTML = `<img src="img/lamp-on.jpg" onclick="onOff()">`
-
+    lamp.innerHTML = `<img src="img/lamp-off.jpg" onclick="onOff()">`   
 }
